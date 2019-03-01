269a4a483c40c4fd78243b66d5bebad87df51350
Sarah Flynn <sarah.flynn@icloud.com>
2019-02-28 19:49:53 -0800

    basic vue-router setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import router from './router';
+import './css/main.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.component('RouterLink', Vue.component('router-link'));
+Vue.component('RouterView', Vue.component('router-view'));
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount('#app');
